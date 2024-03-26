247a2c0b5d577d498bbdc365ba6741e6674563cd
juanbr4 <164948370+juanbr4@users.noreply.github.com>
2024-03-26 13:18:26 +0100

    Update noticia.js

--- js/noticia.js
+++ js/noticia.js
@@ -1,28 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var noticiasContainer = document.getElementById("contenedor-noticias");
-
-  // Reemplaza 'TU_API_KEY' con tu clave de autenticación
-  const apiKey = '3529b74db5414b4796588ed7cba26aa4';
-  const url = 'https://newsapi.org/v2/everything?q=apple&from=2024-03-25&to=2024-03-25&sortBy=popularity&apiKey=3529b74db5414b4796588ed7cba26aa4';
-
-  // Realiza una solicitud a tu API incluyendo la clave de autenticación en el encabezado
-  fetch(url, {
-      headers: {
-          'Authorization': 'Bearer ' + apiKey
-      }
-  })
-  .then(response => response.json())
-  .then(data => {
-      // Procesa los datos recibidos (asumiendo que la estructura es similar al ejemplo JSON mostrado arriba)
-      data.noticias.forEach(noticia => {
-          var noticiaElement = document.createElement("div");
-          noticiaElement.innerHTML = "<h2>" + noticia.titulo + "</h2>" +
-                                     "<p>" + noticia.descripcion + "</p>" +
-                                     "<p>Fecha: " + noticia.fecha + "</p>";
-          noticiasContainer.appendChild(noticiaElement);
+    var noticiasContainer = document.getElementById("contenedor-noticias");
+  
+    let key = "3529b74db5414b4796588ed7cba26aa4";
+    let pais = "us";
+    let url = `https://newsapi.org/v2/top-headlines?country=${pais}&apiKey=${key}`;
+  
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data); 
+        if (data.articles && data.articles.length > 0) {
+          //Solo mostrar 3
+          for (let i = 0; i < 3; i++) {
+            const noticia = data.articles[i];
+            
+            var noticiaElement = document.createElement("div");
+            noticiaElement.classList.add("noticia");
+  
+            
+            var tituloElement = document.createElement("h2");
+            tituloElement.textContent = noticia.title;
+  
+            var descripcionElement = document.createElement("p");
+            descripcionElement.textContent = noticia.description;
+  
+            var fechaElement = document.createElement("p");
+            fechaElement.textContent = "Fecha: " + noticia.publishedAt;
+  
+            //Añadirlo al contenedor
+            noticiaElement.appendChild(tituloElement);
+            noticiaElement.appendChild(descripcionElement);
+            noticiaElement.appendChild(fechaElement);
+  
+            noticiasContainer.appendChild(noticiaElement);
+          }
+        } else {
+          console.error("No se encontraron noticias.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error al cargar las noticias:", error);
       });
-  })
-  .catch(error => {
-      console.error('Error al cargar las noticias:', error);
   });
-});
+  
